cff823478c9b392773d2c8c9e759cb3bb0a5113c
karthik <kkartz@gmail.com>
2018-02-01 08:55:30 +0530

    user implementation first commit

--- EthMeetUser.sol
+++ EthMeetUser.sol
@@ -1,1 +1,20 @@
-undefined
+pragma solidity ^0.4.8;
+
+import "./sharedLibrary.sol";
+import "./strings.sol";
+import "./EthMeetSetter.sol";
+import "./UserLibrary.sol";
+
+contract EthMeetUser is EthMeetSetter {
+    using strings for *;
+    
+    function EthMeetUser(address _ethMeetDB) public {
+        if(_ethMeetDB == 0x0) throw;
+        ethMeetDB = _ethMeetDB;
+    }
+    
+    function setUser(string name, string email) public {
+        UserLibrary.setUser(ethMeetDB, msg.sender, name, email);
+    }
+
+}
