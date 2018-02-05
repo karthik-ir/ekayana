13be7c87937627deeb45ea94e5ef4afbda84945b
karthik <kkartz@gmail.com>
2018-02-05 14:34:47 +0530

    Fixed empty networks in built contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -18,42 +18,40 @@ var Mortal = artifacts.require("./Mortal.sol");
 var SafeMath = artifacts.require("./SafeMath");
 
 module.exports = function(deployer) {
-  deployFactory(deployer).then(function(){
-    return;
-  });
+  deployFactory(deployer);
 };
 
-async function deployFactory(deployer){
-  await deployer.deploy(Ownable);
-  await deployer.link(Ownable, [EthMeetDB,EthMeetSetter, Killable]);
-  await deployer.deploy(Killable);
-  await deployer.link(Killable, Authentication);
-  await deployer.deploy(Authentication);
-  await deployer.deploy(SafeMath);
-  await deployer.link(SafeMath,[SharedLibrary,EthMeetDB])
-  await deployer.deploy(SharedLibrary);
-  await deployer.link(SharedLibrary, [UserLibrary,ListingLibrary,BookingLibrary]);
+function deployFactory(deployer){
+   deployer.deploy(Ownable);
+   deployer.link(Ownable, [EthMeetDB,EthMeetSetter, Killable]);
+   deployer.deploy(Killable);
+   deployer.link(Killable, Authentication);
+   deployer.deploy(Authentication);
+   deployer.deploy(SafeMath);
+   deployer.link(SafeMath,[SharedLibrary,EthMeetDB])
+   deployer.deploy(SharedLibrary);
+   deployer.link(SharedLibrary, [UserLibrary,ListingLibrary,BookingLibrary]);
 
-  await deployer.deploy(EthMeetDB);
-  await deployer.link(EthMeetDB,[UserLibrary,ListingLibrary,BookingLibrary]);
+   deployer.deploy(EthMeetDB);
+   deployer.link(EthMeetDB,[UserLibrary,ListingLibrary,BookingLibrary]);
 
-  await deployer.deploy(UserLibrary);
+   deployer.deploy(UserLibrary);
 
-  await deployer.link(UserLibrary, [ListingLibrary,BookingLibrary, EthMeetSetter]);
-  await deployer.deploy(ListingLibrary);
+   deployer.link(UserLibrary, [ListingLibrary,BookingLibrary, EthMeetSetter]);
+   deployer.deploy(ListingLibrary);
 
-  await deployer.link(ListingLibrary, BookingLibrary);
-  await deployer.deploy(BookingLibrary);
+   deployer.link(ListingLibrary, BookingLibrary);
+   deployer.deploy(BookingLibrary);
   
-  await deployer.deploy(EthMeetSetter);
-  await deployer.link(EthMeetSetter, [EthMeetUser,EthMeetListing, EthMeetBooking]);
+   deployer.deploy(EthMeetSetter);
+   deployer.link(EthMeetSetter, [EthMeetUser,EthMeetListing, EthMeetBooking]);
   
-  await deployer.link(UserLibrary,[EthMeetUser])
-  await deployer.deploy(EthMeetUser, EthMeetDB.address);
+   deployer.link(UserLibrary,[EthMeetUser])
+   deployer.deploy(EthMeetUser, EthMeetDB.address);
   
-  await deployer.link(ListingLibrary,[EthMeetListing])
-  await deployer.deploy(EthMeetListing, EthMeetDB.address);
+   deployer.link(ListingLibrary,[EthMeetListing])
+   deployer.deploy(EthMeetListing, EthMeetDB.address);
 
-  await deployer.link(BookingLibrary,[EthMeetBooking])
-  await deployer.deploy(EthMeetBooking, EthMeetDB.address);
+   deployer.link(BookingLibrary,[EthMeetBooking])
+   deployer.deploy(EthMeetBooking, EthMeetDB.address);
 }
